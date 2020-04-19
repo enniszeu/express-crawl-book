c0095077e4ab4f97e2b0540216648198c442a04d
enniszeu <enniszeu@gmail.com>
2020-04-19 16:10:20 +0700

    gh

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.set('views', './views');
 
 // const db = []
 
-db.defaults({ posts: [], views: [], chaps: [], homeXephang: [], viewsXepHang: [], chapsXephang: []})
+db.defaults({ posts: [], views: [], chaps: [], homeXephang: [], viewsXepHang: [], chapsXephang: [], homeConGai: [], viewsConGai:[], chapsConGai:[]})
   .write()
 
 
@@ -113,15 +113,61 @@ function requestsXephang(){
 	})
 }
 
+function requestsConGai(){
+	request('http://www.nettruyen.com/truyen-con-gai', (err,
+		res, html) =>{
+		if(!err && res.statusCode == 200){
+			const $ = cheerio.load(html)
+			db.get('homeConGai').remove().write()
+
+			$('figure').each((i, el)=>{
+				const imageHome = $(el)
+					.find('.image a img')
+					.attr('data-original')
+
+				const nameHome = $(el)
+					.find('.image a img')
+					.attr('alt')
+
+				const linkHome = $(el)
+					.find('.image a')
+					.attr('href')
+
+				const chapHome = $(el)
+					.find('figcaption ul li a')
+					.attr('title')
+
+				const chapTime = $(el)
+					.find('figcaption ul li i')
+					.text()
+				
+				db.get('homeConGai')
+				  .push({ 
+				  	id : i+1,
+					imageHome : imageHome,
+					nameHome : nameHome.slice(13,500),
+					linkHome : linkHome,
+					chapHome : chapHome,
+					chapTime : chapTime.slice(0,11)
+				  })
+				  .write()
+			})
+
+		}
+	})
+}
 
 
 app.get('/', (req, res)=>{
 
 	requestsHome();
 	requestsXephang();
+	requestsConGai();
+	console.log(db.get('homeConGai').value())
     res.render('pageHome/homePage',{
     	db:db.get('posts').value(),
-    	homeXephang:db.get('homeXephang').value()
+    	homeXephang:db.get('homeXephang').value(),
+    	homeConGai:db.get('homeConGai').value()
     })
 
 })
@@ -328,6 +374,166 @@ app.get('/post/xephang/:id', function(req, res){
     
 })
 
+// viewrequest
+function requestViewConGai(url, id){
+	request({url}, (err,
+		res, html) =>{
+		if(!err && res.statusCode == 200){
+			const $ = cheerio.load(html)
+			db.get('viewsConGai').remove().write()
+
+			const nameView = $('.col-image')
+				.find('img')
+				.attr('alt')
+
+			const imgView = $('.col-image')
+				.find('img')
+				.attr('src')
+
+			const dataView = $('#item-detail')
+			.find('.small')
+			.text()
+			.slice(0,47)
+		
+			const tenKhac = $('.list-info .othername')
+				.find('h2')
+				.text()	
+
+			const tacGia = $('.list-info .author')
+				.find('.col-xs-8')
+				.text()	
+
+			const tinhTrang = $('.list-info .status')
+				.find('.col-xs-8')
+				.text()	
+
+			const theLoai = $('.list-info .kind')
+				.find('.col-xs-8')
+				.text()	
+
+			const noiDung = $('.detail-content')
+				.find('p')
+				.text()	
+
+			console.log(tenKhac)
+			
+				db.get('viewsConGai')
+				  .push({ 
+				  	id:id,
+				  	nameView : nameView,
+					imgView : imgView,
+					dataView : dataView,
+					tenKhac : tenKhac,
+					tacGia : tacGia,
+					tinhTrang : tinhTrang,
+					theLoai : theLoai,
+					noiDung : noiDung
+				  })
+				  .write()
+
+				  $('ul li .chapter').each((i, el)=>{
+					const chapter = $(el)
+						.find('a')
+						.text()
+						.replace(":", "")
+
+					const chapterHref = $(el)
+						.find('a')
+						.attr('href')
+
+
+				db.get('viewsConGai')
+				  .push({ 
+				  	_id : chapter.slice(8,11),
+				  	chapter : chapter,
+				  	chapterHref : chapterHref
+				  })
+				  .write()
+			})
+
+			}
+
+	})
+}
+app.get('/post/congai/:id', function(req, res){
+    
+    const id = req.params.id
+    const items = db.get('homeConGai').find({ id: parseInt(id) }).value()
+    const url = items.linkHome
+    const viewsConGai = db.get('viewsConGai').value()
+
+
+    requestViewConGai(url,id)
+
+    console.log(viewsConGai)
+    res.render('pageViewConGai/ViewConGaiPage',{
+    	viewsConGai:viewsConGai,
+    	id:id,
+    	_id:items.id
+    })
+    
+})
+
+
+app.get('/post/chapcongai/:id', function(req, res){
+    const _id = req.params.id
+    const items = db.get('viewsConGai').find({ _id: _id }).value()
+    const url = items.chapterHref
+
+    
+    var chapsConGai = db.get('chapsConGai').value()
+
+
+    request({url}, (err,
+		res, html) =>{
+		if(!err && res.statusCode == 200){
+			const $ = cheerio.load(html)
+			db.get('chapsConGai').remove().write()
+
+			$('.reading-detail div').each((i, el)=>{
+				const imageChap = $(el)
+					.find('img')
+					.attr('src')
+
+				const nameChap = $(el)
+					.find('img')
+					.attr('src')
+
+				db.get('chapsConGai')
+				  .push({ 
+				  	imageChap : imageChap
+				  })
+				  .write()
+				
+			})
+
+			const nameChap = $('.top')
+				.find('h1')
+				.text()
+
+			const dateChap = $('.top')
+				.find('i')
+				.text()
+
+			db.get('chapsConGai')
+				  .push({ 
+				  	nameChap : nameChap,
+				  	dateChap : dateChap
+				  })
+				  .write()
+
+		}
+	})
+
+    console.log(chapsConGai)
+    res.render('pageViewChapConGai/ViewChapConGaiPage',{
+    	chapsConGai:chapsConGai,
+    	_id : _id
+    })
+    
+})
+
+
 app.get('/post/chapxephang/:id', function(req, res){
     const _id = req.params.id
     const items = db.get('viewsXepHang').find({ _id: _id }).value()
@@ -451,13 +657,29 @@ app.get('/post/chap/:id', function(req, res){
     
 })
 
+app.get('/post', function(req, res){
+	requestsHome();
 
+    res.render('pagePost/PostPage',{
+    	db:db.get('posts').value()
+    })
+    
+})
 
 app.get('/bangxephang', function(req, res){
 	requestsXephang();
-	console.log(db.get('homeXephang').value())
+
     res.render('pageXepHang/XepHangPage',{
     	homeXephang:db.get('homeXephang').value()
+    })
+    
+})
+
+app.get('/congai', function(req, res){
+	requestsConGai();
+
+    res.render('pageConGai/ConGaiPage',{
+    	homeConGai:db.get('homeConGai').value()
     })
     
 })
